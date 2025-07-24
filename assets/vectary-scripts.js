db414a43244c29e00bde435eef25130c3d0b6d6a
roostarmt <102131805+roostarmt@users.noreply.github.com>
2025-07-24 16:32:03 -0600

    .

--- assets/vectary-scripts.js
+++ assets/vectary-scripts.js
@@ -95,7 +95,7 @@ const colorPalette = [
       nameId: "colorNameL1",
       lockId: "lockBG",
       objects: ["wrapkit-partial 1", "wrapkit-full 1"],
-      material: "MAT-Logos-C2", 
+      material: "MAT-Logos-L1", 
     },
   ];
 // Helper function to get color properties from the UI
