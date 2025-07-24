cac85ddb824025abf19a08d82b03c49395e8067f
roostarmt <102131805+roostarmt@users.noreply.github.com>
2025-07-24 11:34:47 -0600

    .

--- assets/vectary-scripts.js
+++ assets/vectary-scripts.js
@@ -297,7 +297,7 @@ const state = {
     C2: 60,
     C1: 80,
     BG: 12,
-    L1: 10, // Default value for Polaris Logos
+    L1: 50, // Default value for Polaris Logos
   },
   activeLevels: new Set(), // Add a Set to track active levels
   activeOverlaminates: new Set(), // Add a Set to track active overlaminates
